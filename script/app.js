f9ce73d692602b2a25d4a148fc2a4ac0695d7a02
Thanabadee Techakasiwattana <bankthanabadee2007@icloud.com>
2021-09-02 16:39:29 +0700

    First commit

--- script/app.js
+++ script/app.js
@@ -1,5 +1,41 @@
-const elementID = document.getElementById('element')
+var modal = document.getElementById("PicModal");
 
-const clicked = () => {
-    elementID.innerHTML = 'Hello'
+var img = document.getElementById("profile-picture");
+var modalImg = document.getElementById("img-Profile");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+const facebook_alert = () => {
+    alert('Open Facebook ?')
+}
+
+const instagram_alert = () => {
+    alert('Open Instagram ?')
+}
+
+const twitter_alert = () => {
+    alert('Open Twitter ?')
+}
+
+const snapchat_alert = () => {
+    alert('Open Snapchat ?')
+}
+
+const tiktok_alert = () => {
+    alert('Open Tiktok ?')
+}
+
+
+
+const backhome = () => {
+    href = './index.html'
 }

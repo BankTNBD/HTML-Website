68ea1d5cb9b7c6c2390007d85f427a10f31948a5
banktnbd <bankthanabadee2007@gmail.com>
2022-03-07 12:30:23 +0700

    refactor code

---
+++ js/app.js
@@ -0,0 +1,27 @@
+urllist = {
+    //social
+    'Facebook': 'https://facebook.com/IamBanky',
+    'Instagram': 'https://instagram.com/iam.banky',
+    'Twitter': 'https://twitter.com/CallMeBanky',
+    'Snapchat': 'https://snapchat.com/add/myname.sbanky',
+    'Github': 'https://github.com/BankTNBD'
+}
+
+const link = (url) => {
+    swal(`Redirect to ${url}?`, {
+        buttons: {
+          no: "Exit",
+          yes: {
+              text: "Redirect",
+              value: "yes"
+          }
+        },
+      })
+      .then((value) => {
+        switch (value) {
+          case "yes":
+            window.location.href = urllist[url]
+            break
+        }
+    })
+}

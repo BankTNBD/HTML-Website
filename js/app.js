8ab6f7f90bed7985b74a79892772da9c59836719
banktnbd <bankthanabadee2007@gmail.com>
2023-01-08 14:06:19 +0700

    Add 52 Weeks project

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ urllist = {
     'Discord': 'https://discord.com/users/784066460627632148',
     'Twitch': 'https://www.twitch.tv/iambanky',
     'Github': 'https://github.com/BankTNBD',
-    'RandomChat': 'http://randomchat.iambanky.com'
+    //Project
+    'RandomChat': 'http://randomchat.iambanky.com',
+    '52WeeksWithThanabadee': 'https://www.instagram.com/52weekswiththanabadee/'
 }
 
 const link = (url) => {

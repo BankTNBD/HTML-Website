aee0a657437dbe0c4b560cf06979b666aae566c3
BankTNBD <bankthanabadee2007@icloud.com>
2023-06-25 18:36:08 +0700

    Add Locket

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@ urllist = {
     'Twitter': 'https://twitter.com/CallMeBanky',
     'Discord': 'https://discord.com/users/784066460627632148',
     'Twitch': 'https://www.twitch.tv/iambanky',
+    'Locket': 'https://locket.camera/links/nbtcM6jfrbeuHq3C6',
     'Github': 'https://github.com/BankTNBD',
+
     //Project
     'RandomChat': 'http://randomchat.iambanky.com',
     '52WeeksWithThanabadee': 'https://www.instagram.com/52weekswiththanabadee/'
